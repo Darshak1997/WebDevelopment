f805d0621fd9b264d18ecdaa0887158209209747
Darshak1997 <42878805+Darshak1997@users.noreply.github.com>
2020-08-16 09:43:54 -0500

    Updated app

--- app.js
+++ app.js
@@ -18,7 +18,8 @@ var commentRoutes = require("./routes/comments"),
     indexRoutes = require("./routes/index")
 
 // seedDB();
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true}); 
+mongoose.connect("mongodb://localhost/yelpCamp", {useNewUrlParser: true});
+// mongoose.connect("mongodb+srv://yelpCamp:Jiraiya_1997@cluster0.mqqel.mongodb.net/<dbname>?retryWrites=true&w=majority", {useNewUrlParser: true}); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
